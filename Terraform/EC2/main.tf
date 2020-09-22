948e6a702d59ada06ce71aa56853c3e88b78ca99
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-09-22 11:47:35 +0100

    Bug fixes.

--- Terraform/EC2/main.tf
+++ Terraform/EC2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Controller" {
   ami                         = var.ami
   instance_type               = var.instance_type
-  key_pair                    = var.key-pair
+  key_pair                    = var.key_pair
   subnet_id                   = var.subnet_id
   vpc_sg_ids                  = [var.vpc_sg_ids]
   tags                        = var.tags
